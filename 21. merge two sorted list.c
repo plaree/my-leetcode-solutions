b85cf0b841e604bc4cf496fb5264cf2b3de0b604
Eralp Demirer <eralp0814@gmail.com>
2023-01-23 20:42:41 +0300

    Create 21. merge two sorted list.c

---
+++ 21. merge two sorted list.c
@@ -0,0 +1,71 @@
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ Example 1:
+ 
+ Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+
+	    struct ListNode *p = NULL, h;
+    
+    if (list1 == NULL)
+        return list2;
+    
+    if (list2 == NULL)
+        return list1;
+    
+    if (list1->val < list2->val) {
+        h.next = list1;
+        list1 = list1->next;
+    } else {
+        h.next = list2;
+        list2 = list2->next;
+    }
+    
+     p = h.next;
+
+    while(list1 && list2) {
+        if (list1->val < list2->val) {
+              p->next = list1;
+              p = list1;
+            list1 = list1->next;
+        } else {
+              p->next = list2;
+              p = list2;
+            list2 = list2->next;
+        }
+    }
+    
+    if (list1)
+          p->next = list1;
+        
+    if (list2)
+          p->next = list2;
+
+    return h.next;
+}
